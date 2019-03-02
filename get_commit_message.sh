128adf6841b147b238a4983094fa43162a8ed80e
mydeveloperday@gmail.com <mydeveloperday@gmail.com>
2019-03-02 10:53:46 +0000

    Curl example and removal of sed

--- get_commit_message.sh
+++ get_commit_message.sh
@@ -1,1 +1,6 @@
-echo '{ "revision_id": '${1:1}' }' | arc call-conduit --conduit-uri https://reviews.llvm.org/ --conduit-token <replace_with_api_token> differential.getcommitmessage | jq -rnf ".response"
+
+# method using arc
+# echo '{ "revision_id": '${1:1}' }' | arc call-conduit --conduit-uri https://reviews.llvm.org/ --conduit-token <replace_with_api_token> differential.getcommitmessage | jq -r ".response" -
+
+# method using curl
+curl -s -S https://reviews.llvm.org/api/differential.getcommitmessage -d api.token=<replace_with_api_token> -d revision_id=${1:1} | jq -r ".result" -
